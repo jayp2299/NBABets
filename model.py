eee6b426cc58568cbe0f8bd7b77bb32b893ad4a1
Jay Phansalkar <jvp66@scarletmail.rutgers.edu>
2021-01-23 19:42:05 -0500

    nba api

--- model.py
+++ model.py
@@ -1,1 +1,13 @@
-print("niggiez")
+from nba_api.stats.endpoints import commonplayerinfo
+from nba_api.stats.endpoints import cumestatsteam
+import json
+
+# Basic Request
+# player_info = commonplayerinfo.CommonPlayerInfo(player_id=2544)
+# print(player_info.available_seasons.get_json())
+# leBron = player_info.available_seasons.get_json()
+# json_object = json.loads(teams.get_teams())
+# json_format = json.dumps(json_object, indent=2)
+# print(json_format)
+
+cumestatsteam.total_team_stats()
